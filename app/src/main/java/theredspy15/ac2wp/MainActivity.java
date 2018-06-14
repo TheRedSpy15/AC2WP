f7db07b3124dd0142cb3fb2f1b239560e9357ba3
TheRedSpy15 <hjadar15@gmail.com>
2018-06-14 16:31:06 -0400

    added ability to save by long press

--- app/src/main/java/theredspy15/ac2wp/MainActivity.java
+++ app/src/main/java/theredspy15/ac2wp/MainActivity.java
@@ -12,12 +12,14 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Looper;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -26,15 +28,20 @@ import com.sdsmdg.tastytoast.TastyToast;
 
 import net.alhazmy13.catcho.library.Catcho;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements ActivityCompat.OnRequestPermissionsResultCallback {
 
-    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 1;
+    private static final int PERMISSIONS_REQUEST_CODE = 1;
 
-    protected static final String[] BASE_PROJECTION = new String[]{MediaStore.Audio.AudioColumns.ALBUM_ID};
+    protected static final String[] BASE_PROJECTION = new String[]{
+            MediaStore.Audio.AudioColumns.ALBUM_ID,
+            MediaStore.Audio.AudioColumns.ALBUM
+    };
     final Uri BASE_URI = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
     final String BASE_SELECTION = MediaStore.Audio.Media.IS_MUSIC + "!= 0";
     final String BASE_SORTORDER = MediaStore.Audio.Media.TITLE + " ASC";
@@ -56,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         originalWallpaper = ((BitmapDrawable) getCurrentWallpaper()).getBitmap();
 
         try {
-            requestReadPermission();
+            requestPermissions();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -92,14 +99,23 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
     private void createImage(Cursor cursor) throws FileNotFoundException {
 
         final Long ALBUM_ID = cursor.getLong(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID));
+        final String ALBUM_NAME = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
 
         final Bitmap ALBUM_COVER = getAlbumCover(ALBUM_ID);
         final ImageView imageView = new ImageView(this);
 
+        // applying
         imageView.setImageBitmap(ALBUM_COVER);
         imageView.setOnClickListener(view -> {
             TastyToast.makeText(this,"Changing",TastyToast.LENGTH_SHORT,TastyToast.DEFAULT).show();
             new Thread(() -> changeWallpaper(ALBUM_COVER)).start();
+        });
+
+        // saving
+        imageView.setOnLongClickListener(view -> {
+            TastyToast.makeText(MainActivity.this,"Saving",TastyToast.LENGTH_SHORT,TastyToast.DEFAULT).show();
+            new Thread(() -> saveCover(ALBUM_COVER,ALBUM_NAME)).start();
+            return true;
         });
 
         albums.addView(imageView);
@@ -138,21 +154,47 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
         return wallpaperManager.getDrawable();
     }
 
-    public void requestReadPermission() throws FileNotFoundException {
+    private void saveCover(Bitmap finalBitmap, String image_name) {
+
+        Looper.prepare();
+
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root);
+        myDir.mkdirs();
+        String fileName = "Image-" + image_name+ ".jpg";
+        File file = new File(myDir, fileName);
+        if (file.exists()) file.delete();
+        Log.i("LOAD", root + fileName);
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            TastyToast.makeText(this,e.getLocalizedMessage(),TastyToast.LENGTH_LONG,TastyToast.ERROR).show();
+        }
+
+        TastyToast.makeText(this,"Done",TastyToast.LENGTH_SHORT,TastyToast.DEFAULT).show();
+
+        Looper.loop();
+    }
+
+    public void requestPermissions() throws FileNotFoundException {
 
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
-        else addAlbumCovers();
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                PERMISSIONS_REQUEST_CODE);
+        else addAlbumCovers(); // granted
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            @NonNull String permissions[], @NonNull int[] grantResults) {
         switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+            case PERMISSIONS_REQUEST_CODE:
                 if (grantResults.length > 0 // Granted
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     try {
