608ce06a56472c3feea1fac838c79b390069deae
TheRedSpy15 <hjadar15@gmail.com>
2018-06-14 23:10:52 -0400

    better permission handling

--- app/src/main/java/theredspy15/ac2wp/MainActivity.java
+++ app/src/main/java/theredspy15/ac2wp/MainActivity.java
@@ -184,6 +184,8 @@ public class MainActivity extends AppCompatActivity implements ActivityCompat.On
 
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
                 != PackageManager.PERMISSION_GRANTED) ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE},
                 PERMISSIONS_REQUEST_CODE);
